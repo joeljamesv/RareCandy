59929604962d432da016eb6b16a344de1f55327c
Joel James <joejamesv7@gmail.com>
2026-03-19 18:15:53 +0000

    Add Snake Head
    Added snake head support which does nothing but spawn random heads
    Looks really stupid Needs rework
    
    Signed-off-by: Joel James <joejamesv7@gmail.com>

--- basicSnakes.cpp
+++ basicSnakes.cpp
@@ -4,7 +4,7 @@ Basic screen,
 
 The free  |    dots(.)
 The food  |    astreix(*)
-The snake |    head greater than (>)
+The snake |    head greater,less, V and PowerOf (>,<,^,V)
 The snake |    body hypher(-)
 */
 
@@ -33,14 +33,58 @@ int main() {
       screen[i][j] = '.';
     }
   }
-  printScreen();
-  // Randomly scatter the Food
-  while (1) {
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    int randomX = rand() % 20;
-    int randomY = rand() % 20;
+  std::cout << "\033[2J\033[H";
 
+  int randomX = rand() % 20;
+  int randomY = rand() % 20;
+  printScreen();
+  // Randomly scatter the food and snake controls
+
+  char snakeHeads[4] = {'<', '>', '^', 'V'};
+  int snakeX = rand() % 20;
+  int snakeY = rand() % 20;
+
+  screen[snakeX][snakeY] = snakeHeads[1];
+  /*
+  Randomly place a snake head at the start
+  listens to 'W' 'A' 'S' 'D'
+  and choose the snake head from there
+  */
+  char userInput;
+  int whichHead;
+  int currentInput;
+  while (1) {
+    std::cin >> userInput;
+    if (userInput == 'w') {
+      whichHead = 2;
+      currentInput = 2;
+    } else if (userInput == 'a') {
+      whichHead = 0;
+      currentInput = 0;
+    } else if (userInput == 's') {
+      whichHead = 3;
+      currentInput = 3;
+    } else if (userInput == 'd') {
+      whichHead = 1;
+      currentInput = 1;
+    }
+    std::cout << "\033[H";
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    screen[randomX][randomY] = '.';
+    randomX = rand() % 20;
+    randomY = rand() % 20;
     screen[randomX][randomY] = '*';
+    screen[snakeX][snakeY] = snakeHeads[whichHead];
     printScreen();
+
+    if (currentInput == 2) {
+      snakeX++;
+    } else if (currentInput == 0) {
+      snakeY--;
+    } else if (currentInput == 3) {
+      snakeX--;
+    } else if (currentInput == 1) {
+      snakeY++;
+    }
   }
 }
